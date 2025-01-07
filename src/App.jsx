5f535cdba73d0e9152812c75169512bac6e0c960
Lana Mesquita <lanabeatriz.mesquita@gmail.com>
2025-01-07 22:58:29 +0000

    feat: Alterando tela inicial

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,22 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+        Jogo da Velha <span className="heart">♥️</span> React | GitHub Codespaces
         </p>
         <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
+          Para fazer o jogo, siga o tutorial link abaixo.
+        </p>
+        <p className="small">
+          Altere o arquivo <code>src/App.jsx</code> e salve para recarregar.
         </p>
         <p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://pt-br.react.dev/learn/tutorial-tic-tac-toe"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Tutorial React: Jogo da Velha
           </a>
         </p>
       </header>
